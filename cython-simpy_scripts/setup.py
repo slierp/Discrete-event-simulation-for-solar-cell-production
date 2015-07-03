c3470f3c76183c744f925329ba165b306bfd6787
slierp <rcgnaber@gmail.com>
2015-07-03 11:39:08 +0200

    Cython simpy scripts for linux

---
+++ cython-simpy_scripts/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+ext_modules=[
+    Extension("_compat", ["_compat.py"]),
+    Extension("core", ["core.py"]),
+    Extension("events", ["events.py"]),
+    Extension("rt", ["rt.py"]),
+    Extension("util", ["util.py"]),
+    Extension("resources.base", ["resources/base.py"]),
+    Extension("resources.container", ["resources/container.py"]),
+    Extension("resources.resource", ["resources/resource.py"]),
+    Extension("resources.store", ["resources/store.py"])
+]
+
+setup(
+	cmdclass = {'build_ext': build_ext},
+	ext_modules = ext_modules
+)
