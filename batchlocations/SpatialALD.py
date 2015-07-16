34cc000c87901f184aab5a8e09e1f3e6829f2c9d
slierp <rcgnaber@gmail.com>
2015-07-16 17:54:37 +0200

    SpatialALD implementation done except downtime

--- batchlocations/SpatialALD.py
+++ batchlocations/SpatialALD.py
@@ -8,7 +8,6 @@ import collections
 """
 TODO
 
-Fix lockup problem with more than few tens of wafers
 Implement downtime
 Implement cassette reload timeout
 
@@ -23,8 +22,7 @@ class SpatialALD(QtCore.QObject):
         self.utilization = []
         
         self.params = {}
-        self.params['specification'] = "DO NOT USE\n"
-        self.params['specification'] += "SpatialALD consists of:\n"
+        self.params['specification'] = "SpatialALD consists of:\n"
         self.params['specification'] += "- Input container\n"
         self.params['specification'] += "- Main conveyor\n"
         self.params['specification'] += "- Deposition units with small input and output buffers\n"
@@ -46,15 +44,15 @@ class SpatialALD(QtCore.QObject):
         
         self.params['time_step'] = 1.0
         self.params['time_step_desc'] = "Time for one wafer to progress one unit distance on main conveyor (seconds)"
+        self.params['time_preheat'] = 3.0
+        self.params['time_preheat_desc'] = "Time to preheat one wafer before deposition (seconds)"        
         self.params['time_process'] = 5.0
-        self.params['time_process_desc'] = "Time to process one wafer including pre-heating (seconds)"
+        self.params['time_process_desc'] = "Time for one deposition process on one wafer (seconds)"
         
         self.params['no_deposition_units'] = 3
         self.params['no_deposition_units_desc'] = "Number of deposition units along the main conveyor"
         self.params['deposition_unit_length'] = 1.0
-        self.params['deposition_unit_length_desc'] = "Distance between input and output position on main conveyor (meters)"        
-        self.params['deposition_buffer_size'] = 3
-        self.params['deposition_buffer_size_desc'] = "Number of units in each deposition unit input buffer"    
+        self.params['deposition_unit_length_desc'] = "Distance between input and output position on main conveyor (meters)"    
 
         self.params['unit_distance'] = 0.2
         self.params['unit_distance_desc'] = "Minimal distance between wafers on belts (meters)"        
@@ -69,6 +67,11 @@ class SpatialALD(QtCore.QObject):
 #            string = str(self.env.now) + " - [SpatialALD][" + self.params['name'] + "] Added a spatial ALD tool" #DEBUG
 #            self.output_text.sig.emit(string) #DEBUG
 
+        ### Give warning for collisons ###
+#        if (self.params['time_step'] > self.params['time_process']):
+#            string = str(self.env.now) + " - [SpatialALD][" + self.params['name'] + "]  <span style=\"color: red\">WARNING: Wafer collisions are likely with present settings</span>"
+#            self.output_text.sig.emit(string)
+
         ### Input ###
         self.input = BatchContainer(self.env,"input",self.params['cassette_size'],self.params['max_cassette_no'])
 
@@ -77,6 +80,7 @@ class SpatialALD(QtCore.QObject):
         # zero denotes empty position; positive value indicates which deposition unit it is intended for (number 1 being the first)
         self.units_on_conveyor = 2+int(self.params['no_deposition_units']*self.params['deposition_unit_length']//self.params['unit_distance'])
         self.conveyor =  collections.deque([0 for rows in range(self.units_on_conveyor)])
+        self.transport_counter = 0
         
         ### Calculate input/output positions for each deposition unit ###
 
@@ -89,67 +93,103 @@ class SpatialALD(QtCore.QObject):
         for i in self.dep_input_positions:
             self.dep_output_positions.append(1 + i + int(self.params['deposition_unit_length']//self.params['unit_distance']))        
         
-        ### Lists for keeping check of wafers in depostion unit buffers ###
+        ### Lists representing deposition unit buffers ###
         
         self.input_buffers = []
         self.output_buffers = []
         
         for i in range(self.params['no_deposition_units']):
-            self.input_buffers.append(collections.deque([0 for rows in range(self.params['deposition_buffer_size'])]))
-            self.output_buffers.append(collections.deque([0 for rows in range(self.params['deposition_buffer_size'])]))
+            self.input_buffers.append(collections.deque([0 for rows in range(2)]))
+            self.output_buffers.append(collections.deque([0 for rows in range(2)]))
+
+        ### Lists representing preheater and deposition area within deposition units ###
+
+        self.dep_unit = []
         
-        ### Request list for wafers by deposition units ###
+        for i in range(self.params['no_deposition_units']):
+            self.dep_unit.append(collections.deque([0 for rows in range(2)]))
         
-        self.request_list = [False for rows in range(self.params['no_deposition_units'])]
+        ### Lists to keep track of wafers on belt ###
+        
+        self.wafers_sent = [0 for rows in range(self.params['no_deposition_units'])]
         
         ### Output ###
         self.output = BatchContainer(self.env,"output",self.params['cassette_size'],self.params['max_cassette_no'])
 
+        ### List for calculating idle times ###
+        self.start_time = []
+        self.first_run = []
+        self.idle_times_internal = []
+        for i in range(self.params['no_deposition_units']):
+            self.idle_times_internal.append(0)
+            self.first_run.append(True)
+            self.start_time.append(0)
+
+        ### Start SimPy processes ###
         self.env.process(self.run_conveyor())      
         self.env.process(self.run_load_in())
         
         for i in range(self.params['no_deposition_units']):
             self.env.process(self.run_buffer_load_in_out(i))
-            self.env.process(self.run_deposition_unit(i)) # count starts at 1 in this case
+            self.env.process(self.run_preheater(i))
+            self.env.process(self.run_deposition_unit(i))
 
     def report(self):
-        string = "[SpatialALD][" + self.params['name'] + "] Units processed: " + str(self.output.container.level)
-        self.output_text.sig.emit(string)        
+        string = "[SpatialALD][" + self.params['name'] + "] Units processed: " + str(self.transport_counter)
+        self.output_text.sig.emit(string)
+
+        ### Report utilization ###        
+        self.utilization.append("SpatialALD")
+        self.utilization.append(self.params['name'])
+        self.utilization.append(self.nominal_throughput())
+        production_volume = self.transport_counter
+        production_hours = (self.env.now - self.start_time[0])/3600
+        
+        if (self.nominal_throughput() > 0) & (production_hours > 0):
+            self.utilization.append(round(100*(production_volume/production_hours)/self.nominal_throughput(),1))
+        else:
+            self.utilization.append(0)            
+        
+        for i in range(len(self.idle_times_internal)):
+            if self.first_run[i]:
+                idle_time = 100.0
+            elif ((self.env.now-self.start_time[i]) > 0):
+                idle_time = 100.0*self.idle_times_internal[i]/(self.env.now-self.start_time[i])
+            self.utilization.append(["du" + str(i),round(idle_time,1)])         
 
     def prod_volume(self):
-        return self.output.container.level
+        return self.transport_counter
     
     def run_load_in(self):
+
+        wafer_ready = False
         no_deposition_units = self.params['no_deposition_units']
-        preference_list = collections.deque([False for rows in range(self.params['no_deposition_units'])])
-        preference_list[0] = True
+        priority_list = collections.deque([False for rows in range(no_deposition_units)])
+        priority_list[-1] = True
         
-        while True:                       
-            
-            if (not self.conveyor[0]):
-
+        while True:                               
+                
+            if not wafer_ready:
                 yield self.input.container.get(1)
-               
-                while True:
-                    preferred = 0
-                    for i in range(len(preference_list)):
-                        if preference_list[i]:
-                            preferred = i
-                    
-                    # spread wafers evenly over the deposition units
-                    if self.request_list[preferred]:
-                        self.conveyor[0] = preferred+1 # number indicates which DU it is intended for
-                        self.request_list[preferred] = False # request granted so need to place new request
-                        preference_list.rotate(1) # following DU will preferably get next wafer
+                wafer_ready = True
+            
+            for i in range(no_deposition_units):
 
-#                        if (self.params['verbose']): #DEBUG     
-#                            string = str(self.env.now) + " - [SpatialALD][" + self.params['name'] + "] Placed wafer on conveyor belt" #DEBUG
-#                            self.output_text.sig.emit(string) #DEBUG                        
-                        
-                        break
-                    else:
-                        preference_list.rotate(1)
-                        
+                p = next(j for j, x in enumerate(priority_list) if x) # find current priority in list
+                
+                if (self.input_buffers[p].count(0) > self.wafers_sent[p]):
+                    self.conveyor[0] = p+1 # number indicates which DU it is intended for
+                    wafer_ready = False # need new wafer
+                    self.wafers_sent[p] += 1 # keep track of what is on the conveyor
+                    
+#                    if (self.params['verbose']): #DEBUG     
+#                        string = str(self.env.now) + " - [SpatialALD][" + self.params['name'] + "] Placed wafer on conveyor belt for DU " + str(p) #DEBUG
+#                        self.output_text.sig.emit(string) #DEBUG                    
+                    
+                    break
+                else:
+                    priority_list.rotate(-1) # next deposition unit now has priority            
+            
             yield self.env.timeout(self.time_step)
     
     def run_conveyor(self):
@@ -162,6 +202,7 @@ class SpatialALD(QtCore.QObject):
                 # automatically performs load-out
                 self.conveyor[-1] = 0
                 yield self.output.container.put(1)
+                self.transport_counter += 1
             
             yield self.env.timeout(self.time_step)
 
@@ -179,16 +220,15 @@ class SpatialALD(QtCore.QObject):
             if (self.conveyor[input_pos] == (num+1)):
                 # get wafers intended for this deposition unit
                 self.conveyor[input_pos] = 0
+                self.wafers_sent[num] -= 1
                 self.input_buffers[num][0] = num+1
 
 #                if (self.params['verbose']): #DEBUG     
 #                    string = str(self.env.now) + " - [SpatialALD][" + self.params['name'] + "][du" + str(num) + "] Received one wafer" #DEBUG
-#                    self.output_text.sig.emit(string) #DEBUG                
-                
-            elif (self.input_buffers[num][0] == 0):
-                self.request_list[num] = True
+#                    self.output_text.sig.emit(string) #DEBUG
 
             if (self.output_buffers[num][-1] == 0):
+                # move wafers forward towards main conveyor
                 self.output_buffers[num].rotate(1)
             else:                            
                 # put processed wafers back onto main conveyor
@@ -198,17 +238,45 @@ class SpatialALD(QtCore.QObject):
             
             yield self.env.timeout(self.time_step)        
 
+    def run_preheater(self,num):
+
+        time_preheat = self.params['time_preheat']
+
+        while True:
+            
+            if (self.input_buffers[num][-1] > 0) & (self.dep_unit[num][0] == 0):                
+                # preheat wafer if wafer and room available                
+                
+                self.input_buffers[num][-1] = 0
+                yield self.env.timeout(self.time_step)                
+                yield self.env.timeout(time_preheat)                    
+                self.dep_unit[num][0] = num+1
+
+#                if (self.params['verbose']): #DEBUG     
+#                    string = str(self.env.now) + " - [SpatialALD][" + self.params['name'] + "][du" + str(num) + "] Preheated one wafer" #DEBUG
+#                    self.output_text.sig.emit(string) #DEBUG
+                
+            else:
+                yield self.env.timeout(self.time_step)
+
     def run_deposition_unit(self,num):
 
         time_process = self.params['time_process']
         
-        while True:
+        while True:                
+
+            if self.first_run[num]:
+                self.start_time[num] = self.env.now
+                self.first_run[num] = False  
             
-            if (self.input_buffers[num][-1] > 0):
-                # process wafer if available                
-                self.input_buffers[num][-1] = 0
-                yield self.env.timeout(time_process)
+            if (self.dep_unit[num][0] > 0) & (self.dep_unit[num][-1] == 0):                
+                # perform deposition if wafer and room is available
+            
+                self.dep_unit[num].rotate(1)
+                yield self.env.timeout(self.time_step)
+                yield self.env.timeout(time_process)                    
                 self.output_buffers[num][0] = num+1
+                self.dep_unit[num][-1] = 0
 
 #                if (self.params['verbose']): #DEBUG     
 #                    string = str(self.env.now) + " - [SpatialALD][" + self.params['name'] + "][du" + str(num) + "] Processed one wafer" #DEBUG
@@ -216,6 +284,9 @@ class SpatialALD(QtCore.QObject):
                 
             else:
                 yield self.env.timeout(self.time_step)
+                
+                if not self.first_run[num]:
+                    self.idle_times_internal[num] += self.time_step                
     
     def nominal_throughput(self):
 
